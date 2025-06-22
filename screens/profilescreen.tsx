b95e04578436a93742eba130ec853b6cb30d20a9
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-22 15:56:48 +0100

    feat: profile modules complete

---
+++ screens/profilescreen.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/components/Restyle";
+import React from "react";
+
+const ProfileScreen = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">Profile Screen</Text>
+    </Box>
+  );
+};
+
+export default ProfileScreen;
